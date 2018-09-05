490badb094705e5cc6b228698968fa4afb3e5cc4
Edward <edward_song@yeah.net>
2018-09-05 17:18:09 +0800

    1st commit

---
+++ HA_Weekends/frmMain.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace HA_Weekends
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.AllowDrop = true;
+            this.dateTimePicker1.CalendarFont = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(32, 46);
+            this.dateTimePicker1.MinDate = new System.DateTime(2018, 8, 1, 0, 0, 0, 0);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(136, 22);
+            this.dateTimePicker1.TabIndex = 0;
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(648, 400);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Name = "frmMain";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+    }
+}
+
---
+++ HA_Weekends/frmMain.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+
+namespace HA_Weekends
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        string url = @"http://api.goseek.cn/Tools/holiday?date=";
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        
+
+
+
+        /// <summary>
+        /// 获得今年有几周
+        /// </summary>
+        /// <param name="year"></param>
+        /// <returns></returns>
+        public int GetWeekOfYear(int year)
+        {
+            DateTime the_Date = new DateTime(year, 1, 1);//本年的第一天
+            TimeSpan tt = the_Date.AddYears(1) - the_Date;//求出本年有几天
+            return tt.Days / 7 + 1; //因为年只有366天和365天除以7有余数所以始终需要加一周
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string HttpGet(string url)
+        {
+            StreamReader reader = null;
+            try
+            {
+                //ServicePointManager.ServerCertificateValidationCallback = new RemoteCertificateValidationCallback(CheckValidationResult);
+                Encoding encoding = Encoding.UTF8;
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+                request.Method = "GET";
+                request.Accept = "text/html, application/xhtml+xml, */*";
+                request.ContentType = "application/json";
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                using (reader = new StreamReader(response.GetResponseStream(), Encoding.UTF8))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+               // WriteLog.CreateExceptionLog(ex);
+                return reader.ReadToEnd();
+            }
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public class HolidayDate
+        {
+            public int Code { set; get; }
+            public int Date { set; get; }
+        }
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public enum DateType
+        {
+            WORKDATE =0,//工作日
+            WEEKEND = 1,//周末休息日
+            HOLIDAY = 2//法定假日(国假类)
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sdate"> 日期的格式,20180901</param>
+        /// <returns></returns>
+        private DateType  GetDateType(string sdate)
+        {
+            DateType dt = new DateType();
+            string response = HttpGet(url + sdate);
+            HolidayDate holidaydate = JsonConvert.DeserializeObject<HolidayDate>(response);
+            dt = (DateType)holidaydate.Date;
+            return dt;
+        }
+
+
+
+
+
+    
+
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            string _sdate = dateTimePicker1.Value.ToString("yyyyMMdd");
+
+            //MessageBox.Show(GetDateType(_sdate).ToString());
+
+            
+        }
+    }
+}
